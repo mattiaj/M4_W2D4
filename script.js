dbfb1f1805c3deab9be622561f724bcda83d7a07
mattiaj <mattiajavatto@gmail.com>
2024-02-14 19:51:31 +0100

    Esercizio completo

--- script.js
+++ script.js
@@ -3,10 +3,27 @@ fetch("https://striveschool-api.herokuapp.com/books")
 .then((json) => createTemplate(json))
 .catch((err) => console.log("Error detected: ", err) );
 
-let container = document.getElementById('bookContainer');
-let cart = document.getElementById("cart");
-let searchInput = document.getElementById('search-book');
+const container = document.getElementById('container');
+const containerBook = document.getElementById('bookContainer');
+const cart = document.getElementById("cart");
+const searchInput = document.getElementById('search-book');
+const emptyCart = document.getElementById('empty-cart');
 let activeResult;
+
+// fetch sul singolo libro
+const bookEndPoint = "https://striveschool-api.herokuapp.com/books/";
+if(window.location.search) {
+    let activeParams = window.location.search;
+    let objParam = new URLSearchParams(activeParams);
+    let bookId = objParam.get("id");
+
+fetch("https://striveschool-api.herokuapp.com/books/"+bookId)
+.then((res) => res.json())
+.then((json) => createBookInfo(json))
+.catch((err) => console.log("Error detected: ", err) );
+}
+
+
 // console.log(searchInput)
 
 function createTemplate (books, savedResult = true) {
@@ -25,6 +42,10 @@ function createCard (element) {
         card.classList.add('card', 'col-lg-3', 'col-md-4', 'col-12', 'px-0');
         card.style.backgroundColor = "#444242"
         card.id = asin;
+        let link = document.createElement('a');
+        link.href =  `index.html?id=${card.id}`;
+        link.innerText = "info";
+        link.classList.add('text-light', 'link-underline','link-underline-opacity-0')
         let cardImg = document.createElement('img');
         cardImg.src = img;
         cardImg.classList.add('card-img-top');
@@ -51,20 +72,23 @@ function createCard (element) {
         let cardTextBody = [cardTitle, cardCategory,cardJump, footerCard];
         cardBody.append( ...cardTextBody);
         // variabile che contiene il footer della card
-        let cardTextFooter = [cardPrice, buttonCart];
+        let cardTextFooter = [cardPrice, link, buttonCart];
         footerCard.append(...cardTextFooter);
         // creazione effettiva della card
         card.append(cardImg, cardBody, footerCard);
         // aggiungo la card al DOM
-        container.appendChild(card);
+        containerBook.appendChild(card);
         // console.log(buttonCart)
 
-
+        cardJump.addEventListener('click', () => {
+            card.remove()
+        });
+        
         buttonCart.addEventListener('click', ()=>{
             cart.append(cartElement(element));
             card.classList.add('border', 'border-danger')
-        })
-    
+        });
+        
     
 }
 
@@ -82,6 +106,7 @@ function cartElement(element) {
     let cardTitle = document.createElement('h6');
     let cardPrice = document.createElement('p');
     let buttonRemove = document.createElement('a');
+    
 
     card.classList.add('card', 'col-12', 'px-0', 'mt-2');
     cardImg.classList.add('card-img-top');
@@ -109,13 +134,19 @@ function cartElement(element) {
         card.remove();
     })
 
+    // svuota carrello
+    emptyCart.addEventListener('click', () => {
+        card.remove();
+    })
+
+
     return card;
 }
 
 // funzione per la ricerca
 function research () {
     if (activeResult) {
-        container.innerHTML = "";
+        containerBook.innerHTML = "";
         let liveSearch = searchInput.value;
         let filteredResult = activeResult.filter((books) => {
             return books.title.toLowerCase().includes(liveSearch.toLowerCase());
@@ -124,4 +155,41 @@ function research () {
 
         createTemplate(filteredResult, false)
     }
+}
+
+function createBookInfo(element) {
+    container.innerHTML = "";
+    let title = element.title;
+    let img = element.img;
+    let category = element.category;
+    let price = element.price;
+    let text = "QUESTO LIBRO È BELLO! COMPRALO SUBITO!"
+
+    let cardTitleBig = document.createElement('h1');
+    let cardTitle = document.createElement('h2');
+    let cardImg = document.createElement('img');
+    let cardText = document.createElement('h5');
+    let cardCategory = document.createElement('h4');
+    let cardPrice = document.createElement('h6');
+    let divImgText = document.createElement('div');
+    let divtext = document.createElement('div');
+    let divContainer = document.createElement('div');
+
+    cardTitleBig.innerText = title;
+    cardTitle.innerText = title;
+    cardImg.src = img;
+    cardText.innerText = text;
+    cardCategory.innerText = category;
+    cardPrice.innerText = "€" + price;
+
+    cardImg.style.maxHeight = "500px"
+    divtext.classList.add('text-light', 'mx-3');
+    divImgText.classList.add('d-flex', );
+    cardTitleBig.classList.add('text-light', 'text-center', 'mb-5', 'mt-4');
+    cardText.classList.add('my-5');
+
+    divtext.append(cardTitle, cardText, cardCategory, cardPrice);
+    divImgText.append(cardImg, divtext);
+    divContainer.append(cardTitleBig, divImgText);
+    container.append(divContainer);
 }
